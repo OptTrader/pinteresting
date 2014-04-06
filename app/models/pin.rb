a56557350190c861ba92840b252724c86d98cfa7
Your Name <your.email@example.com>
2014-04-06 16:46:15 +0800

    latest

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
   
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :default_url => "https://ckpinteresting.amazons3.com/:style/missing.png" }
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	
 end
