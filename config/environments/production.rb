5341b6bf07a270a20b314a2cfc09b8b987611fef
Your Name <your.email@example.com>
2014-04-09 19:09:12 +0800

    latest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,6 @@ Pinteresting::Application.configure do
       :bucket => ENV['AWS_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    :s3_protocol    => "https"                                             ,
-    :s3_host_name   => "s3-eu-west-1.amazonaws.com"
     }
   }
 
